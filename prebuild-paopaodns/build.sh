000cf4fcea85dcf86b73ffabe654cf29327b3951
SnakWu <snakwu@gmail.com>
2026-09-07 17:11:02 +0800

    Update build.sh

--- prebuild-paopaodns/build.sh
+++ prebuild-paopaodns/build.sh
@@ -3,19 +3,19 @@
 # add tools
 apk update
 apk upgrade
-apk add build-base flex byacc musl-dev gcc make git python3-dev swig libevent-dev openssl-dev expat-dev hiredis-dev go grep bind-tools
+apk add --no-cache build-base flex byacc musl-dev gcc make git python3-dev swig libevent-dev openssl-dev expat-dev hiredis-dev go grep bind-tools
 
 # build unbound
 #git clone https://github.com/NLnetLabs/unbound.git --depth 1 /unbound -b release-1.19.3
 git clone https://github.com/NLnetLabs/unbound.git --depth 1 /unbound
 cd /unbound || exit
-export CFLAGS="-O3"
+export CFLAGS="-O3 -march=alderlake -mtune=alderlake"
 ./configure --with-libevent --with-pthreads --with-libhiredis --enable-cachedb \
     --disable-rpath --without-pythonmodule --disable-documentation \
     --disable-flto --disable-maintainer-mode --disable-option-checking --disable-rpath \
     --with-pidfile=/tmp/unbound.pid \
     --prefix=/usr --sysconfdir=/etc --localstatedir=/tmp --with-username=root --with-chroot-dir=""
-make
+make -j"$(nproc)"
 make install
 mv /usr/sbin/unbound /src/
 mv /usr/sbin/unbound-checkconf /src/
@@ -24,7 +24,7 @@ mv /usr/sbin/unbound-checkconf /src/
 mkdir -p /mosdns-build
 git clone https://github.com/kkkgo/mosdns --depth 1 /mosdns-build
 cd /mosdns-build || exit
-go build -ldflags "-s -w" -trimpath -o /src/mosdns
+GOAMD64=v3 go build -ldflags "-s -w" -trimpath -o /src/mosdns
 
 #clean
 rm /src/build.sh
